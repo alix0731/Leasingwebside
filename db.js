2b1f52dd1ef7e35c0f6cd53e127abdedad3a86b2
omar0190 <61865216+omar0190@users.noreply.github.com>
2021-05-17 13:22:04 +0200

    a

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql");
+require("dotenv").config();
 
 
 const connection = mysql.createConnection({
-host: "den1.mysql3.gear.host",
-user: "nodeleasing", 
-password: 	"Su0vU9~v_Y1v",
-database: "nodeleasing"
+host: process.env.DB_HOST,
+user: process.env.DB_USER, 
+password: process.env.DB_PASSWORD,
+database: process.env.DB_DATABASE
 });
 
 module.exports = {
