b038c3769066c21a39ee721e021ec872b8455e09
Ali Raza Akhtar <alirazaakhtar21@hotmail.com>
2021-05-17 03:21:14 +0200

    fjenet alt unødvendigt

--- app.js
+++ app.js
@@ -2,13 +2,38 @@ const { static } = require("express");
 const express = require("express");
 const app = express();
 
+const fs = require("fs");
+
 app.use(express.json());
 app.use(express.static("public"));
 
 
+//header
+const header = fs.readFileSync(__dirname + "/public/header/header.html", "utf-8");
+//footer
+const footer = fs.readFileSync(__dirname + "/public/footer/footer.html", "utf-8");
+
+
+//homepage
+const home = fs.readFileSync(__dirname + '/public/frontpage/home.html', "utf-8");
+//cars
+const cars = fs.readFileSync(__dirname + "/public/cars/cars.html", "utf-8");
+//contact
+const contact = fs.readFileSync(__dirname + "/public/contact/contact.html", "utf-8");
+
+
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + '/public/frontpage/frontpage.html')
+    res.send(header + home + footer);
 });
+
+app.get("/biler", (req, res) => {
+    res.send(header + cars + footer);
+});
+
+app.get("/kontakt", (rew, res) => {
+    res.send(header + contact + footer);
+});
+
 
 
 app.listen(8080, (error)=>{
