688850cca293ad1d3194fa708e16fec8fd040568
omar0190 <61865216+omar0190@users.noreply.github.com>
2021-06-13 23:48:18 +0200

    mailing

--- routes/contact.js
+++ routes/contact.js
@@ -11,7 +11,7 @@ var transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
         user: process.env.EMAIL,
-        pass: process.env.MAIL_PASSWORD
+        pass: process.env.EMAIL_PASSWORD
     }
 });
 
